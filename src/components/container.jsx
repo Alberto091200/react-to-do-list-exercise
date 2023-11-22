c6000fc28876dafb1e19a24db54e839fbf9c08b9
unknown <alobato9@gail.com>
2023-11-22 10:40:07 +0100

    to do list

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import FormTodo from "./formTodo";
 import TaskList from "./taskList";
 
 const Container = () => {
+    const [list, setList] = useState([])
+    const handleAddItem = addItem => {
+        setList([...list,addItem])
+    }
+    
+
     return (
         <div>
             Container!
-            <FormTodo/>
-            <TaskList/>
+            <FormTodo handleAddItem = {handleAddItem}/>
+            <TaskList list = {list} setList = {setList}/>
         </div>
     )
 }
--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import Checkbox from "./Checkbox";
 
-import CheckBox from "./checkBox";
+const TaskList = props => {
+  const { list, setList } = props;
 
-const TaskList = () => {
-    return(
-         <div>
-            TaskList
-            <CheckBox/>
-        </div>
-    )
-}
+  const onChangeStatus = e => {
+    const { name, checked } = e.target;
 
-export default TaskList
+    const updateList = list.map(item => ({
+      ...item,
+      done: item.id === name ? checked : item.done
+    }));
+    setList(updateList);
+  };
+
+  const onClickRemoveItem = e => {
+    const updateList = list.filter(item => !item.done);
+    setList(updateList);
+  };
+
+  const chk = list.map(item => (
+    <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
+  ));
+  return (
+    <div className="todo-list">
+      {list.length ? chk : "No tasks"}
+      {list.length ? (
+        <p>
+          <button className="button blue" onClick={onClickRemoveItem}>
+            Delete
+          </button>
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default TaskList;
